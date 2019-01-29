d468e9373276687d15e1ee9436a189ecedfc58c5
Jeff Gu Kang <jeffgukang@gmail.com>
2019-01-29 12:10:18 +0900

    Add getItemList

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -1,11 +1,23 @@
-Parse.Cloud.define('hello functions', function(req, res) {
+Parse.Cloud.define('hello functions', function(req) {
   return 'Hi functions';
 });
 
-Parse.Cloud.define('whoami', function(req, res) {
+Parse.Cloud.define('whoami', (req) => {
   const name = req.params.name;
   const money = req.params.money;
 
   const result = `Hello. ${name}! You will earn $${money}!!!`
   return result;
+});
+
+Parse.Cloud.define('getItemList', async (req) => {
+  const Character = Parse.Object.extend('Character'); // Get class from database
+  const query = new Parse.Query(Character); // Make query from class
+
+  try {
+    const result = await query.find(); // Get all datas
+    return result;
+  } catch (error) {
+    throw error;
+  }
 });
